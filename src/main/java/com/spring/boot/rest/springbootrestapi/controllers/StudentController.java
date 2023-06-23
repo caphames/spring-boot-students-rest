c90edec76915bfe90ff2bea4201cb01640b91944
caphames <71441690+caphames@users.noreply.github.com>
2023-06-23 16:49:53 +0530

    Student Controller Sample API

--- src/main/java/com/spring/boot/rest/springbootrestapi/controllers/StudentController.java
+++ src/main/java/com/spring/boot/rest/springbootrestapi/controllers/StudentController.java
@@ -1,2 +1,22 @@
-package com.spring.boot.rest.springbootrestapi.controllers;public class StudentController {
+package com.spring.boot.rest.springbootrestapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of(
+                "Hammad",
+                "Kahsif",
+                 "Umair"
+        );
+    }
 }
